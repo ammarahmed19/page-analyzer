d416dfac616d287b295108ac9f600167cb14e127
Mora-Hannover <ammaribrahim0166@gmail.com>
2019-02-05 00:56:52 +0200

    First Test

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from urllib.request import urlopen
+import re
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("index.html")
+
+@app.route("/search", methods=["POST"])
+def search():
+    words = list(map(lambda x : x.strip() ,list(map(lambda x: x.lower(), request.form.get("Phrases/Words").split(',')))))
+    pglink = request.form.get("Page Link")
+    found = []
+    message = "Here are the highlited words/phrases: "
+    try:
+        pghandle = urlopen(str(pglink).strip())
+    except:
+        return "Link is invalid"
+    pgtext = pghandle.read()
+    for word in words:
+        pattern = r"\W?{}\W?".format(word)
+        if re.search(pattern, str(pgtext).lower().strip()):
+            found.append(word)
+    
+    for word in found:
+        message += "<br>g" + word 
+
+    if found:
+        return message
+    else:
+        return "No words/phrases were found matching your criteria"
+    #return "Words: " + str(words) + '\n' + "Page Link:" + str(pglink)
+
 if __name__ == "__main__":
     app.run(debug=True)
